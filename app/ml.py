92c25d4505c18ae0d7bdb4235f8c8efb9b3fe243
Animesh Risal <animeshrisal99@gmail.com>
2022-01-17 15:35:57 +0545

    Add fixes

--- app/ml.py
+++ app/ml.py
@@ -7,6 +7,17 @@ from .models import Movie
 def combined_feature(row):
     return row['genres'] + " " + row['keywords'] + " " + row['tagline'] + " " + row['cast'] + " " + row['director'] + " " + row['overview']
 
+df = pd.DataFrame(list(Movie.objects.all().values()))
+features = ['keywords', 'cast', 'genres', 'director', 'overview']
+for feature in features:
+    df[feature] = df[feature].fillna('')
+    
+df["combined_features"] = df.apply(combined_feature, axis=1)
+
+cv = CountVectorizer()
+count_matrix = cv.fit_transform(df["combined_features"])
+cosine_sim = cosine_similarity(count_matrix)
+
 def get_id_from_index(df, index):
     return df[df.index == index]["id"].values[0]
 
@@ -14,17 +25,6 @@ def get_index_from_id(df, id):
     return df[df.id == id].index.values[0]
 
 def get_recommendation_for_movie(movie_id):
-
-    df = pd.DataFrame(list(Movie.objects.all().values()))
-    features = ['keywords', 'cast', 'genres', 'director', 'overview']
-    for feature in features:
-        df[feature] = df[feature].fillna('')
-        
-    df["combined_features"] = df.apply(combined_feature, axis=1)
-
-    cv = CountVectorizer()
-    count_matrix = cv.fit_transform(df["combined_features"])
-    cosine_sim = cosine_similarity(count_matrix)
 
     id = get_index_from_id(df, movie_id)
 
@@ -38,7 +38,7 @@ def get_recommendation_for_movie(movie_id):
         i = i + 1
         movie_ids.append(get_id_from_index(df, movie[0]))
         
-        if i > 5:
+        if i > 15:
             break
 
     return movie_ids
