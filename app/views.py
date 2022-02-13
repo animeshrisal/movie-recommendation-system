dd3705ba6f89eb28e3c18b0f9af0391afb05a94c
Animesh Risal <animeshrisal99@gmail.com>
2022-02-13 07:31:28 +0545

    Lint views

--- app/views.py
+++ app/views.py
@@ -17,12 +17,16 @@ from django.db import transaction
 from rest_framework import generics
 
 # Create your views here.
+
+
 def home_page(request):
     return render(request, 'home.html')
     # return HttpResponse("Welcome to django class")
 
+
 def about_page(request):
     return render(request, 'about.html')
+
 
 def get_movies(request, page_number):
     if page_number < 1:
@@ -35,8 +39,10 @@ def get_movies(request, page_number):
     }
 
     page_size = 10
-    movies = Movie.objects.all()[(page_number-1)*page_size:page_number*page_size]
+    movies = Movie.objects.all()[(page_number-1)
+                                 * page_size:page_number*page_size]
     return render(request, 'movies.html', {'movies': movies, 'pagination': pagination})
+
 
 def update_movie(request, id):
     movie = Movie.objects.get(pk=id)
@@ -57,31 +63,31 @@ def get_movie_details(request, id):
         review_form = ReviewForm(request.POST)
         if review_form.is_valid():
             review = review_form.save(commit=False)
-            review.movie_id= id
+            review.movie_id = id
             review.user_id = request.user.id
-            review.save()    
+            review.save()
 
     movie = Movie.objects.get(id=id)
     reviews = Review.objects.filter(movie=movie).order_by('-created_at')[0:4]
-    context = { 
+    context = {
         'is_favorite': False
     }
 
     if movie.favorite.filter(pk=request.user.pk).exists():
         context['is_favorite'] = True
 
-
     movie_ids = get_recommendation_for_movie(id)
 
     recommended_movies = Movie.objects.filter(id__in=movie_ids)
-    
-    return render(request, 'movie_description.html', 
-    {'movie': movie, 
-    'context': context, 
-    'reviews': reviews, 
-    'review_form': review_form,
-    'recommended_movies': recommended_movies
-    })
+
+    return render(request, 'movie_description.html',
+                  {'movie': movie,
+                   'context': context,
+                   'reviews': reviews,
+                   'review_form': review_form,
+                   'recommended_movies': recommended_movies
+                   })
+
 
 def post_movie(request):
     form = MovieForm()
@@ -96,6 +102,7 @@ def post_movie(request):
 
     return render(request, 'post_movie.html', {'form': form})
 
+
 def signup(request):
     form = UserCreationForm()
     if request.method == "POST":
@@ -103,12 +110,13 @@ def signup(request):
 
         if form.is_valid():
             form.save()
-            user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+            user = authenticate(
+                username=form.cleaned_data['username'], password=form.cleaned_data['password'])
             login(request, user)
             return redirect('/movie/')
 
-
     return render(request, 'signup.html', {'form': form})
+
 
 def signin(request):
     form = AuthenticationForm()
@@ -118,10 +126,10 @@ def signin(request):
         print(form.is_valid())
         print(form.errors)
         if form.is_valid():
-            user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+            user = authenticate(
+                username=form.cleaned_data['username'], password=form.cleaned_data['password'])
             login(request, user)
             return redirect('/movie/')
-
 
     return render(request, 'signin.html', {'form': form})
 
@@ -130,21 +138,25 @@ def signout(request):
     logout(request)
     return redirect('/movie/')
 
+
 def add_to_favorite(request, id):
     movie = Movie.objects.get(id=id)
     movie.favorite.add(request.user)
 
     return redirect('/movie/{0}'.format(id))
 
+
 def remove_from_favorites(request, id):
     movie = Movie.objects.get(id=id)
     movie.favorite.remove(request.user)
-    
+
     return redirect('/movie/{0}'.format(id))
 
+
 def get_user_favorites(request):
-    movies =request.user.favorite.all()
+    movies = request.user.favorite.all()
     return render(request, 'user_favorite.html', {'movies': movies})
+
 
 def upload_dataset(request):
     file_form = UploadForm()
@@ -160,15 +172,15 @@ def upload_dataset(request):
                 with transaction.atomic():
                     for index, row in dataset.iterrows():
                         movie = Movie(
-                            title = row['title'],
-                            budget = row['budget'],
-                            genres = row['genres'],
-                            keywords = row['keywords'],
-                            overview = row['overview'],
-                            tagline = row['tagline'],
-                            cast = row['cast'],
-                            director = row['director']
-                       )
+                            title=row['title'],
+                            budget=row['budget'],
+                            genres=row['genres'],
+                            keywords=row['keywords'],
+                            overview=row['overview'],
+                            tagline=row['tagline'],
+                            cast=row['cast'],
+                            director=row['director']
+                        )
 
                         new_movies_list.append(movie)
 
@@ -176,11 +188,9 @@ def upload_dataset(request):
         except Exception as e:
             error_messages['error'] = e
 
-
     return render(request, 'upload_dataset.html', {'form': file_form, 'error_messages': error_messages})
+
 
 class RetrieveMovieList(generics.ListAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
-
-
